733e6fc2a54523c2179db5b1d72b60f73b03d16f
PCCLONBCN\Alumno <aa>
2023-03-06 14:07:09 +0100

    Musica_Zooms_EasterEgg

---
+++ Assets/Scripts/Audio/AudioSwap3.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwap3 : MonoBehaviour
+{
+    public AudioClip newTrack;
+    public AudioManager2 am2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Rock"))
+        {
+            am2.track01.Stop();
+            am2.track02.Stop();
+            AudioManager3.instance.SwapTrack(newTrack);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AudioManager3.instance.ReturnToTrack1();
+        }
+    }
+
+}
