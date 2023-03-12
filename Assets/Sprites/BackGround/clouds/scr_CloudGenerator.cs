69c627e5319a272661bad7f2f4de1316a527c16f
Superpila92 <55327225+Superpila92@users.noreply.github.com>
2023-03-12 13:19:57 +0100

    Particulas_GeneradorNubes_PequeñosCambios

---
+++ Assets/Sprites/BackGround/clouds/scr_CloudGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_CloudGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] clouds;
+
+    [SerializeField]
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject endPoint;
+
+    Vector3 startPos;
+
+    public float distance_01;
+    public float distance_02;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        SpawnCloud();
+        Invoke("AttemptSpawn", spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnCloud()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, clouds.Length);
+        GameObject cloud = Instantiate(clouds[randomIndex]);
+
+        cloud.transform.position = startPos;
+
+        startPos.y = UnityEngine.Random.Range(distance_01, distance_02);
+        cloud.transform.position = startPos;
+
+        float scale = UnityEngine.Random.Range(6f, 12f);
+        cloud.transform.localScale = new Vector2(scale, scale);
+
+
+        float speed = UnityEngine.Random.Range(2f, 12f);
+        cloud.GetComponent<scr_Cloud>().StartFloating(speed, endPoint.transform.position.x);
+
+    }
+    void AttemptSpawn()
+    {
+        SpawnCloud();
+
+
+        Invoke("AttemptSpawn", spawnInterval);
+    }
+}
