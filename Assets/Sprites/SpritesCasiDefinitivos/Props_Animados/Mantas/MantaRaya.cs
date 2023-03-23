449f7b14998dabc9240a69ba2bceb47942f4cc3d
PCCLONBCN\Alumno <aa>
2023-03-23 10:57:19 +0100

    MantaRaya

---
+++ Assets/Sprites/SpritesCasiDefinitivos/Props_Animados/Mantas/MantaRaya.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantaRaya : MonoBehaviour
+{
+    public float _speed = 6f;
+    private float _endPosX;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * (Time.deltaTime * _speed)); 
+
+        //if(transform.position.x > _endPosX)
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+    public void StartFloating(float speed, float endPosX)
+    {
+        _speed = speed;
+        _endPosX = endPosX;
+    }
+
+}
