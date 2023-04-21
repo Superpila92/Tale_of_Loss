859d551f0c16c67c0d7fc61e140a20c46903717a
Superpila92 <55327225+Superpila92@users.noreply.github.com>
2023-04-21 15:59:03 +0200

    ChekeoDiario_MusicaMenu_ArregloProps&Bugs

---
+++ Assets/Scripts/Audio/AudioManager4.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager4 : MonoBehaviour
+{
+    public AudioClip mainTrack;
+
+    private AudioSource track01, track02;
+    public bool isPlayingTrack01;
+
+    public static AudioManager4 instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        track01 = gameObject.AddComponent<AudioSource>();
+        track02 = gameObject.AddComponent<AudioSource>();
+        isPlayingTrack01 = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwapTrack(AudioClip newClip)
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(FadeTrack(newClip));
+        isPlayingTrack01 = !isPlayingTrack01;
+    }
+    public void ReturnToTrack1()
+    {
+        SwapTrack(mainTrack);
+    }
+    private IEnumerator FadeTrack(AudioClip newClip)
+    {
+        float timeToFade = 0.25f;
+        float timeElapsed = 0;
+
+        if (isPlayingTrack01)
+        {
+            track02.clip = newClip;
+            track02.Play();
+
+            while(timeElapsed < timeToFade)
+            {
+                track02.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                track01.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            track01.Stop();
+        }
+        else
+        {
+            track01.clip = newClip;
+            track01.Play();
+
+            while(timeElapsed < timeToFade)
+            {
+                track01.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                track02.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            track02.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/AudioSwap4.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwap4 : MonoBehaviour
+{
+    public AudioClip newTrack;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            AudioManager4.instance.SwapTrack(newTrack);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AudioManager4.instance.ReturnToTrack1();
+        }
+    }
+}
