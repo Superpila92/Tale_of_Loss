9a6fa59ba98b9c1887c464d1e34e074825b4e7ba
PCCLONBCN\Alumno <aa>
2023-05-10 16:51:11 +0200

    llamita corta

--- Assets/Scripts/HideMouse.cs
+++ Assets/Scripts/HideMouse.cs
@@ -6,14 +6,14 @@ public class HideMouse : MonoBehaviour
 {
     private void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
