9363ce2206acf019e1ab35c3b0ead860dc9952eb
PCCLONBCN\Alumno <aa>
2023-05-31 18:31:56 +0200

    Mariposas

--- Assets/Scripts/HideMouse.cs
+++ Assets/Scripts/HideMouse.cs
@@ -6,23 +6,23 @@ public class HideMouse : MonoBehaviour
 {
     private void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
     }
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+    //    if (Input.GetKey(KeyCode.Mouse0))
+    //    {
+    //        Cursor.lockState = CursorLockMode.Locked;
+    //        Cursor.visible = false;
+    //    }
     }
 }
